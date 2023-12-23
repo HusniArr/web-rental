31d06911f62318013e39bc05c8d45d929167470c
arief kurniawan <arief.siklus@gmail.com>
2023-12-23 11:39:05 +0700

    set baseUrl

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 define('ROOT', dirname(__DIR__)); // c://laragon/www/oop-mvc
 define('DS', DIRECTORY_SEPARATOR); // /
-define('APP_URL', 'http://localhost/oop-mvc/public');// http://localhost/oop/mvc/public
+define('APP_URL', 'http://localhost/web-rental/public');// http://localhost/oop/mvc/public
 define('DIR', str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT.DS));// Folder name
 
 session_start();
